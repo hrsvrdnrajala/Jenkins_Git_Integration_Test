b419e6572286151c69b0bfa7d1463839b2ecf54c
Harsha Rajala <hrs.vrdn.rajala@gmail.com>
2019-07-28 12:36:56 +0530

    Added another line of code to display the output

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
 class Test{
 	public static void main(String args[]) throws InterruptedException{
 	Thread.sleep(10000);
-	System.out.println("Hello Jenkins");
+	System.out.println("Hello \n");
+	System.out.println("Congrats JEN, on integration with GIT");
 	}
 }
